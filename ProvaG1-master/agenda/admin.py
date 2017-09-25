909e4c09ac7f83a627f71bd387abe9e043f6228b
lucasparaiba <30608598+lucasparaiba@users.noreply.github.com>
2017-09-25 19:57:24 -0300

    Add files via upload

---
+++ ProvaG1-master/agenda/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from agenda.models import *
+admin.site.register(Usuario)
+admin.site.register(AgendaPrivada)
+admin.site.register(AgendaPublica)
+admin.site.register(AgendaInstitucional)
+admin.site.register(Compromissos)
+# Register your models here.
